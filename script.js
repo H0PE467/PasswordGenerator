b91269b6c445d0d66b1a5a3747e0bb6254d6cd35
Klaus Cedillo <klausmcedillo@gmail.com>
2022-03-13 21:47:23 -0600

    Add Concatenation

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ function writePassword() {
 
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
 function generatePassword() {
   passwLenght = window.prompt("How long will your password be? (8-124)")
   passwLenght = Number(passwLenght)
@@ -38,6 +34,116 @@ function generatePassword() {
   addNumeric = window.confirm("Add numeric values in your password?")
   addSpecialChar = window.confirm("Add special characters in your password?")
 
+
+  var selectedCharacters = 0;
+  var tempPassw = "";
+
+  if (addLowerCase==false && addUpperCase==false && addNumeric==false && addSpecialChar==false) {
+    return "Error, select at least one parameter";
+  }
+
+  while (true) {
+
+    //Add Lowercase
+
+    if (addLowerCase == true) {
+
+      if (getRandomInt(0,2)==1 ) {
+        tempPassw = tempPassw + lowerCase[getRandomInt(0,26)];
+
+        selectedCharacters = selectedCharacters + 1;
+        
+        if (selectedCharacters == passwLenght) {
+          break
+        }
+      }
+      else{
+        tempPassw = lowerCase[getRandomInt(0,26)] + tempPassw;
+
+        selectedCharacters = selectedCharacters + 1;
+        
+        if (selectedCharacters == passwLenght) {
+          break
+        }
+      }
+    }
+    
+    //Add Uppercase
+
+    if (addUpperCase == true) {
+
+      if (getRandomInt(0,2)==1 ) {
+        tempPassw = tempPassw + upperCase[getRandomInt(0,26)];
+
+        selectedCharacters = selectedCharacters + 1;
+        
+        if (selectedCharacters == passwLenght) {
+          break
+        }
+      }
+      else{
+        tempPassw = upperCase[getRandomInt(0,26)] + tempPassw;
+
+        selectedCharacters = selectedCharacters + 1;
+        
+        if (selectedCharacters == passwLenght) {
+          break
+        }
+      }
+    }
+
+    //Add Numbers
+
+    if (addNumeric == true) {
+
+      if (getRandomInt(0,2)==1 ) {
+        tempPassw = tempPassw + numbers[getRandomInt(0,10)];
+
+        selectedCharacters = selectedCharacters + 1;
+        
+        if (selectedCharacters == passwLenght) {
+          break
+        }
+      }
+      else{
+        tempPassw = numbers[getRandomInt(0,10)] + tempPassw;
+
+        selectedCharacters = selectedCharacters + 1;
+        
+        if (selectedCharacters == passwLenght) {
+          break
+        }
+      }
+    }
+
+    //Add Special Characters
+
+    if (addSpecialChar == true) {
+
+      if (getRandomInt(0,2)==1 ) {
+        tempPassw = tempPassw + specialChar[getRandomInt(0,3)];
+
+        selectedCharacters = selectedCharacters + 1;
+        
+        if (selectedCharacters == passwLenght) {
+          break
+        }
+      }
+      else{
+        tempPassw = specialChar[getRandomInt(0,3)] + tempPassw;
+
+        selectedCharacters = selectedCharacters + 1;
+        
+        if (selectedCharacters == passwLenght) {
+          break
+        }
+      }
+    }
+  }
+
+  return tempPassw;
+
+  
 }
 
 // Add event listener to generate button
